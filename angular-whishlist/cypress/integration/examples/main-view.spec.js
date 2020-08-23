9fb6c70b7516b2cbb35db8cac096e3d356068e29
Juan Camilo <juacostar@unal.edu.co>
2020-08-23 18:48:00 -0500

    Ultimo commit

--- angular-whishlist/cypress/integration/examples/main-view.spec.js
+++ angular-whishlist/cypress/integration/examples/main-view.spec.js
@@ -1,10 +1,33 @@
 const { createYield } = require("typescript")
 
+
+// primer test
 describe('ventana principal', () => {
     it('tiene encabezado correcto y español por defecto', () => {
         cy.visit('http://localhost:4200');
         cy.contains('angular whishlist');
         cy.get('h1 b').should('contain', 'HOLA es');
+
+    })
+})
+
+
+// segundo test
+describe('ventana principal', () => {
+    it('tiene la etiqueta fecha bien', () => {
+        cy.visit('http://localhost:4200');
+        cy.contains('angular whishlist');
+        cy.get('hr').should('contain', 'Fecha');
+
+    })
+})
+
+// tercer test
+describe('ventana principal', () => {
+    it('link home corrrecto', () => {
+        cy.visit('http://localhost:4200');
+        cy.contains('angular whishlist');
+        cy.get('a').should('contain', 'Home');
 
     })
 })
